663e046a950339470343da27736b7bf671d3ebf2
Theodore Chiu <theochiu.me@gmail.com>
2021-09-08 22:26:25 -0400

    added memory of 100 messages, messages must be after program start

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import string
 
 # groupme access token (api access)
 token = r'06lbAWJcU77H9lyCNVSSuVQfpzl4GVx32880yGAr'
+processed = []
 
 # we'll use this later
 announcements_groupid = r'69765237'
@@ -18,6 +19,7 @@ announcements_groupid = r'69765237'
 # constants specific to bot testing chat
 groupid = r'70744704'
 bot_id = r'6cce054c14e89aeea59b2315b9'
+start_time = int(time.time())
 
 def send(endpoint, data):
 	response = requests.post(endpoint, json=data, verify=False)
@@ -35,14 +37,16 @@ def send_message(message):
 def get_message():
 	# gets most recent message
 	response = requests.get("https://api.groupme.com/v3/groups/" + groupid + "/messages?token=" + token)
-
+	# print(response.json())
 	m_time = response.json()["response"]["messages"][0]["created_at"]
 	message = response.json()["response"]["messages"][0]["text"]
 	sender = response.json()["response"]["messages"][0]["sender_id"]
+	m_id = response.json()["response"]["messages"][0]["id"]
 
-	return m_time, message, sender
+	return m_time, message, sender, m_id
 
 def dm(sender, message):
+	print("violation detected: sending DM")
 	rand_string = "".join(random.choice(string.ascii_lowercase) for i in range(10))
 	data = {
 		"direct_message": {
@@ -56,19 +60,23 @@ def dm(sender, message):
 	print(response.text)
 
 def main():
-	m_time, message, sender = get_message()
+	global processed
+	m_time, message, sender, m_id = get_message()
 
 	# get unix time
 	tic = int(time.time())
 
 	elapsed = (tic - m_time) / 60
 
-	if elapsed <= 5 and "#announcement" not in message:
-		dm(sender, "This is an automated response. Are you sure that was an announcement? Please preface announcements with \#anouncement. If the message was sent in the wrong chat by mistake, kindly delete it")
+	if elapsed <= 5 and "#announcement" not in message and m_id not in processed and m_time > start_time:
+		dm(sender, "This is an automated response. Are you sure that was an announcement? Include #announcement in the future. If the message was sent in the wrong chat by mistake, kindly delete it")
+		processed.append(m_id)
+		if (len(processed) > 100):
+			processed = processed[50:]
 
 if __name__ == '__main__':
 	print("Jarvis is running")
 	while(1):
 		main()
-		# send_message("Jarvis here...")
+
 	
